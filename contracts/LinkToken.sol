4aaa189cb5cd948a63507f82ac02fb20a9709390
kelvin <okorieebube1@gmail.com>
2021-12-03 18:42:28 +0100

    tests: writing for the token contract

--- contracts/LinkToken.sol
+++ contracts/LinkToken.sol
@@ -4,10 +4,30 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract LinkToken {
+    string public name = "Link Token";
+    string public symbol = "LINK";
+    string public version = "v1.0";
+
     uint256 public totalSupply;
-    mapping (address => uint256) public balanceOf;
+
+    mapping(address => uint256) public balanceOf;
+
     constructor(uint256 _initalSupply) {
         balanceOf[msg.sender] = _initalSupply;
         totalSupply = _initalSupply;
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value, "Insufficient funds");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/LinkToken-test.js
+++ test/LinkToken-test.js
@@ -3,11 +3,40 @@ const { ethers } = require("hardhat");
 
 describe("LinkToken", function () {
   before(async () => {
+    [admin, investor1, investor2, investor3] = await ethers.getSigners();
     LinkToken = await ethers.getContractFactory("LinkToken");
     linkToken = await LinkToken.deploy(1000000);
     await linkToken.deployed();
   });
-  it("Should return the token total supply", async function () {
+  it("should setup token contract with correct values", async function () {
+    expect(await linkToken.name()).to.equal("Link Token");
+    expect(await linkToken.symbol()).to.equal("LINK");
+  });
+  it("should mint token supply to admin on deployment", async function () {
     expect(await linkToken.totalSupply()).to.equal(1000000);
+    expect(await linkToken.balanceOf(admin.address)).to.equal(1000000);
+  });
+
+  it("should allow transfer of tokens", async function () {
+    await expect(
+      linkToken.transfer(investor1.address, 999999999)
+    ).to.be.revertedWith("Insufficient funds");
+    let transfer = await linkToken.callStatic.transfer(investor1.address, 100);
+    expect(transfer).to.equal(true);
+    let txn = await linkToken.transfer(investor1.address, 100);
+    let reciept = await txn.wait();
+
+    // Triggers one event
+    expect(reciept.events.length).to.equal(1);
+    // Event triggered should be the transfer event
+    expect(reciept.events[0].event).to.equal("Transfer");
+    // Event logs the correct required arguments
+    expect(reciept.events[0].args._from).to.equal(admin.address);
+    expect(reciept.events[0].args._to).to.equal(investor1.address);
+    expect(reciept.events[0].args._value).to.equal(100);
+
+    // Deducts & adds funds from intended accounts
+    expect(await linkToken.balanceOf(investor1.address)).to.equal(100);
+    expect(await linkToken.balanceOf(admin.address)).to.equal(999900);
   });
 });
